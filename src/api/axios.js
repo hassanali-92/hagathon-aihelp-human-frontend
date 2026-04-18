b157f7bf510fd88a1fed1f628154e700f1e9ed06
hassanali-92 <hassan.programmer92@gmail.com>
2026-04-19 04:23:48 +0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // Dono options rakhein: local aur production (env se handle karein)
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://hagathon-aihelp-human-backend.onrender.com/',
     timeout: 30000, 
     headers: {
         'Content-Type': 'application/json'
